3b67748978e005ca54781b605fbdd09056573c25
joseph <bscs155j2022@students.tum.ac.ke>
2024-10-01 11:26:40 +0300

    changes to crud

--- crudProject/urls.py
+++ crudProject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.item_list, name='item_list'),
+    path('create/', views.item_create, name='item_create'),
+    path('<int:id>/edit/', views.item_update, name='item_update'),
+    path('<int:id>/delete/', views.item_delete, name='item_delete'),
 ]
